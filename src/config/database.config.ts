55b32ad311599681925d877ec3aeae032ee1f42f
JoseIPoletto <polettojose_@hotmail.com>
2025-07-04 17:22:01 -0300

    Actualiza la configuración de la base de datos: se modifican las credenciales de acceso y se cambia el host por uno nuevo.

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,10 +2,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'backend-pi-ej3a.onrender.com',
+  host: process.env.DB_HOST || 'dpg-d1k3e1p5pdvs73abh74g-a',
   port: parseInt(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'Poletto_Jose',
-  password: process.env.DB_PASSWORD || 'Super-contraseña-segura-2025',
+  username: process.env.DB_USERNAME || 'poletto_jose',
+  password: process.env.DB_PASSWORD || 'ZuX8zkYso29lS1Vw8iCjWH2IEgTrfJjbI',
   database: process.env.DB_NAME || 'backend_pi',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production',
