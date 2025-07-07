a36c21a5187eb4ab1ca4f871a5c98e81c77cea26
JoseIPoletto <polettojose_@hotmail.com>
2025-07-07 15:13:05 -0300

    comit a developer

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,13 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'dpg-d1k3e1p5pdvs73abh74g-a',
+  host: process.env.DB_HOST || 'dpg-d1k3e1p5pdvs73abh74g-a.oregon-postgres.render.com',
   port: parseInt(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'poletto_jose',
-  password: process.env.DB_PASSWORD || 'ZuX8zkYso29lS1Vw8iCjWH2IEgTrfJjbI',
+  password: process.env.DB_PASSWORD || 'ZuX8zkYso29lS1Vw8iCjWH2IgTrfJjbI',
   database: process.env.DB_NAME || 'backend_pi',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production',
   logging: process.env.NODE_ENV !== 'production',
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-}; 
+};
