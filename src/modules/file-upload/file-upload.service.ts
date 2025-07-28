2a0990a34259ec6abc608c96407c98a5661b6ece
JoseIPoletto <polettojose_@hotmail.com>
2025-07-28 15:18:48 -0300

    fix(dependencies): agregar @types/multer a las dependencias en package.json y corregir la importación de Repository en file-upload.service.ts

--- src/modules/file-upload/file-upload.service.ts
+++ src/modules/file-upload/file-upload.service.ts
@@ -1,9 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { FileUpLoadRepository } from './file.upload.repository';
 import { User } from '../users/user.entity';
-
+import { Repository } from 'typeorm/repository/Repository';
 @Injectable()
 export class FileUploadService {
   constructor(
