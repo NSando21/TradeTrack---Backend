6b0cb8baf84255d82d225d5ae051d08e86f873e0
JoseIPoletto <polettojose_@hotmail.com>
2025-07-30 19:35:09 -0300

    fix(trips.module.ts): corregir la ruta de importación del módulo de productos para que sea relativa

--- src/modules/trips/trips.module.ts
+++ src/modules/trips/trips.module.ts
@@ -7,7 +7,7 @@ import { Trip } from './trip.entity';
 import { Provider } from '../providers/Entities/provider.entity';
 import { ProviderPicture } from '../providers/Entities/provider-pictures.entity';
 import { User } from '../users/user.entity';
-import { Product } from '@/products/entities/product.entity';
+import { Product } from '../../products/entities/product.entity';
 import { NotificationsModule } from '../notifications/notifications.module'; // Importar módulo de notificaciones
 
 @Module({
